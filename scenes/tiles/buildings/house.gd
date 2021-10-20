c1509a69853a7d046382d99eecb3730381ad4757
Krzysztof Jankowski <krzysztof.jankowski@beffio.com>
2021-10-20 21:11:59 +0200

    right side road + house trash fixes

--- scenes/tiles/buildings/house.gd
+++ scenes/tiles/buildings/house.gd
@@ -19,23 +19,34 @@ func init_thrash_bin(type):
 	if type == self.THRASH_PLASTIC:
 		$SM_TrashBin_Yellow.visible = true
 	if type == self.THRASH_PAPER:
-		$SM_TrashBin_Green.visible = true
-	if type == self.THRASH_GLASS:
 		$SM_TrashBin_Blue.visible = true
-			
+	if type == self.THRASH_GLASS:
+		$SM_TrashBin_Green.visible = true
+
 
 func plant_thrash():
 	self.has_thrash = true
 	self.thrash_level += 1
 
-	if self.type == self.THRASH_METAL:
-		$"SM_TrashBin_Blue/RootNode/SM_Bin_Blue/SM_Trash_Blue".visible = true
-	if self.type == self.THRASH_PLASTIC:
-		$"SM_TrashBin_Yellow/RootNode/SM_Bin_Yellow/SM_Trash_Yellow".visible = true
-	if self.type == self.THRASH_PAPER:
-		$"SM_TrashBin_Blue/RootNode/SM_Bin_Blue/SM_Trash_Blue".visible = true
-	if self.type == self.THRASH_GLASS:
-		$"SM_TrashBin_Green/RootNode/SM_Bin_Green/SM_Trash_Green".visible = true
+	if self.thrash_level > 0:
+		if self.type == self.THRASH_METAL:
+			$SM_TrashBin_Red.visible = true
+		if self.type == self.THRASH_PLASTIC:
+			$SM_TrashBin_Yellow.visible = true
+		if self.type == self.THRASH_PAPER:
+			$SM_TrashBin_Blue.visible = true
+		if self.type == self.THRASH_GLASS:
+			$SM_TrashBin_Green.visible = true
+	
+	if self.thrash_level > 1:
+		if self.type == self.THRASH_METAL:
+			$"SM_TrashBin_Red/RootNode/SM_Bin_Red/SM_Trash_Red".visible = true
+		if self.type == self.THRASH_PLASTIC:
+			$"SM_TrashBin_Yellow/RootNode/SM_Bin_Yellow/SM_Trash_Yellow".visible = true
+		if self.type == self.THRASH_PAPER:
+			$"SM_TrashBin_Blue/RootNode/SM_Bin_Blue/SM_Trash_Blue".visible = true
+		if self.type == self.THRASH_GLASS:
+			$"SM_TrashBin_Green/RootNode/SM_Bin_Green/SM_Trash_Green".visible = true
 
 	if self.thrash_level > 2:
 		$Exclamation.visible = true
@@ -48,3 +59,7 @@ func empty_thrash():
 	$"SM_TrashBin_Yellow/RootNode/SM_Bin_Yellow/SM_Trash_Yellow".visible = false
 	$"SM_TrashBin_Blue/RootNode/SM_Bin_Blue/SM_Trash_Blue".visible = false
 	$"SM_TrashBin_Green/RootNode/SM_Bin_Green/SM_Trash_Green".visible = false
+	$SM_TrashBin_Red.visible = false
+	$SM_TrashBin_Yellow.visible = false
+	$SM_TrashBin_Green.visible = false
+	$SM_TrashBin_Blue.visible = false
