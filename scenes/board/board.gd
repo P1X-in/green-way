df91b8c38ce2568ff0698ee8237b5a3bf1c7996c
Wojciech Chojnacki <wojciech.chojnacki@pearson.com>
2021-10-20 11:10:36 +0200

    Autotiling roads

--- scenes/board/board.gd
+++ scenes/board/board.gd
@@ -75,3 +75,67 @@ func end_game(winner):
 
 func place_road(position):
     self.map.builder.place_ground(position, self.map.templates.ROAD_STRAIGHT, 0)
+
+    var tile = self.map.model.get_tile(position)
+
+    self.replace_road_tile(tile)
+    for neighbour in tile.neighbours.values():
+        self.replace_road_tile(neighbour)
+
+func replace_road_tile(tile):
+    if not tile.ground.is_present():
+        return
+
+    var mask = 0
+
+    mask += self.should_connect_neighbour(tile, "n", 1)
+    mask += self.should_connect_neighbour(tile, "s", 2)
+    mask += self.should_connect_neighbour(tile, "e", 4)
+    mask += self.should_connect_neighbour(tile, "w", 8)
+
+    match mask:
+        0:
+            return
+        1:
+            return
+        2:
+            return
+        3:
+            return
+        4:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_STRAIGHT, 90)
+        5:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_BEND, 180)
+        6:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_BEND, 90)
+        7:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_T, 180)
+        8:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_STRAIGHT, 90)
+        9:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_BEND, 270)
+        10:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_BEND, 0)
+        11:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_T, 0)
+        12:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_STRAIGHT, 90)
+        13:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_T, 270)
+        14:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_T, 90)
+        15:
+            self.map.builder.place_ground(tile.position, self.map.templates.ROAD_CROSS, 0)
+
+
+func should_connect_neighbour(tile, neighbour_key, value):
+    var neighbour = tile.get_neighbour(neighbour_key)
+
+    if neighbour == null:
+        return 0
+
+    if neighbour.ground.is_present():
+        return value
+
+    return 0
+
--- scenes/map/tile.gd
+++ scenes/map/tile.gd
@@ -11,8 +11,6 @@ var frame = preload("res://scenes/map/tile_fragment.gd").new()
 var decoration = preload("res://scenes/map/tile_fragment.gd").new()
 var terrain = preload("res://scenes/map/tile_fragment.gd").new()
 var building = preload("res://scenes/map/tile_fragment.gd").new()
-var unit = preload("res://scenes/map/tile_fragment.gd").new()
-var damage = preload("res://scenes/map/tile_fragment.gd").new()
 
 var fragments = []
 
@@ -28,8 +26,6 @@ func _init(x, y):
         self.decoration,
         self.terrain,
         self.building,
-        self.unit,
-        self.damage,
     ]
 
 func has_content():
@@ -45,24 +41,14 @@ func get_dict():
         "decoration" : self.decoration.get_dict(),
         "terrain" : self.terrain.get_dict(),
         "building" : self.building.get_dict(),
-        "unit" : self.unit.get_dict(),
-        "damage" : self.damage.get_dict(),
     }
 
 func wipe():
-    self.unit.clear()
     self.building.clear()
     self.terrain.clear()
     self.decoration.clear()
     self.frame.clear()
     self.ground.clear()
-    self.damage.clear()
-
-func is_selectable(side):
-    if self.unit.is_present():
-        return self.unit.tile.side == side
-    elif self.building.is_present():
-        return self.building.tile.side == side
 
     return false
 
@@ -81,114 +67,8 @@ func is_neighbour(tile):
             return true
     return false
 
-
-func can_acommodate_unit():
-    if not self.ground.is_present():
-        return false
-    if self.unit.is_present():
-        return false
-    if self.building.is_present():
-        return false
-    if self.terrain.is_present():
-        return self.terrain.tile.unit_can_stand
-
-    return true
-
-func can_pass_through(side):
-    if not self.ground.is_present():
-        return false
-    if self.building.is_present():
-        return false
-    if self.has_enemy_unit(side):
-        return false
-    if self.terrain.is_present():
-        return self.terrain.tile.unit_can_stand
-
-    return true
-
-func has_enemy_unit(side):
-    if self.unit.is_present() && self.unit.tile.side != side:
-        return true
-    return false
-
-func has_friendly_unit(side):
-    if self.unit.is_present() && self.unit.tile.side == side:
-        return true
-    return false
-
-func has_enemy_building(side):
-    if self.building.is_present() && self.building.tile.side != side:
-        return true
-    return false
-
-func has_friendly_building(side):
-    if self.building.is_present() && self.building.tile.side == side:
-        return true
-    return false
-
-func neighbours_enemy_unit(side):
-    for direction in self.neighbours.keys():
-        if self.neighbours[direction].has_enemy_unit(side):
-            return true
-    return false
-
-func can_attack_neightbour_enemy_unit(attacking_unit):
-    for direction in self.neighbours.keys():
-        if self.neighbours[direction].has_enemy_unit(attacking_unit.side):
-            if attacking_unit.can_attack(self.neighbours[direction].unit.tile):
-                return true
-    return false
-
-func neighbours_enemy_building(side):
-    for direction in self.neighbours.keys():
-        if self.neighbours[direction].has_enemy_building(side):
-            return true
-    return false
-
 func get_direction_to_neighbour(tile):
     for direction in self.neighbours.keys():
         if self.neighbours[direction] == tile:
             return direction
     return null
-
-func can_unit_interact(interacting_unit):
-    if not interacting_unit.has_moves():
-        return false
-
-    if self.has_enemy_unit(interacting_unit.side) && interacting_unit.can_attack(self.unit.tile) && interacting_unit.has_attacks():
-        return true
-
-    if self.has_enemy_building(interacting_unit.side) && interacting_unit.can_capture:
-        return true
-
-    return false
-
-func has_friendly_hq(side):
-    if self.building.is_present() && self.building.tile.side == side && self.building.tile.template_name in ["modern_hq", "steampunk_hq", "futuristic_hq", "feudal_hq"]:
-        return true
-    return false
-
-func has_friendly_hero(side):
-    if self.has_friendly_unit(side) && self.unit.tile.unit_class == "hero":
-        return true
-    return false
-
-func is_ground_damage_possible():
-    if not self.ground.is_present():
-        return false
-    if self.unit.is_present():
-        return false
-    if self.building.is_present():
-        return false
-    if self.terrain.is_present():
-        return false
-    if self.damage.is_present():
-        return false
-
-    return true
-
-func is_object_damage_possible():
-    return self.terrain.is_present() and self.terrain.tile.is_damageable()
-
-func is_damageable():
-   return self.is_ground_damage_possible() or self.is_object_damage_possible()
