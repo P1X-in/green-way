fcd13c50e4daa49e0910256e3c132843d601df91
czlowiekimadlo <czlowiekimadlo@gmail.com>
2021-10-21 15:06:34 +0200

    Integrated how to scene

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -10,10 +10,10 @@ func _input(event):
         get_tree().quit()
 
     if event is InputEventMouseButton and event.pressed:
-        return self.get_tree().change_scene("res://scenes/board/board.tscn")
+        return self.get_tree().change_scene("res://scenes/HowTo.tscn")
 
     if event is InputEventJoypadButton and event.pressed:
-        return self.get_tree().change_scene("res://scenes/board/board.tscn")
+        return self.get_tree().change_scene("res://scenes/HowTo.tscn")
 
     if event is InputEventKey:
-        return self.get_tree().change_scene("res://scenes/board/board.tscn")
+        return self.get_tree().change_scene("res://scenes/HowTo.tscn")
