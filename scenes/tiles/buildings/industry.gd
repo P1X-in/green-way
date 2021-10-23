69a93fee21b1b3fee5253677008e1bba58730afb
Krzysztof Jankowski <krzysztof.jankowski@beffio.com>
2021-10-23 13:59:49 +0200

    all fbx-es are gone; hail gltf-s

--- scenes/tiles/buildings/industry.gd
+++ scenes/tiles/buildings/industry.gd
@@ -5,26 +5,26 @@ var type
 var templates = load("res://scenes/map/templates.gd").new()
 
 func is_truck_free():
-	return self.truck.is_truck_free()
+    return self.truck.is_truck_free()
 
 func set_type(_type):
-	self.type = _type
-	self.truck.set_type(_type)
-	print(_type)
-	if _type == self.templates.THRASH_METAL:
-		$"Building_Industry/RootNode/Red".visible = true
-		$Red.visible = true
-	if _type == self.templates.THRASH_PLASTIC:
-		$"Building_Industry/RootNode/Yellow".visible = true
-		$Yellow.visible = true
-	if _type == self.templates.THRASH_PAPER:
-		$"Building_Industry/RootNode/Blue".visible = true
-		$Blue.visible = true
-	if _type == self.templates.THRASH_GLASS:
-		$"Building_Industry/RootNode/Green".visible = true
-		$Green.visible = true
+    self.type = _type
+    self.truck.set_type(_type)
+    print(_type)
+    if _type == self.templates.THRASH_METAL:
+        $"SM_Building_Industry/Red".visible = true
+        $Red.visible = true
+    if _type == self.templates.THRASH_PLASTIC:
+        $"SM_Building_Industry/Yellow".visible = true
+        $Yellow.visible = true
+    if _type == self.templates.THRASH_PAPER:
+        $"SM_Building_Industry/Blue".visible = true
+        $Blue.visible = true
+    if _type == self.templates.THRASH_GLASS:
+        $"SM_Building_Industry/Green".visible = true
+        $Green.visible = true
 
 func dispatch_truck(destination_tile, path, directions, return_directions, board):
-	self.truck.dispatch(destination_tile, path, directions, return_directions, board)
+    self.truck.dispatch(destination_tile, path, directions, return_directions, board)
 
 
