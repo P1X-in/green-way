4433ca5bb2e71ecfc7f1219c97a907ae6fbf705a
Krzysztof Jankowski <krzysztof.jankowski@beffio.com>
2021-10-23 21:32:54 +0200

    full hd

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -3,17 +3,17 @@ extends Spatial
 onready var audio = $"/root/SimpleAudioLibrary"
 
 func _ready():
-    self.audio.track("menu")
+	self.audio.track("menu")
 
 func _input(event):
-    if Input.is_key_pressed(KEY_ESCAPE):
-        get_tree().quit()
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
 
-    if event is InputEventMouseButton and event.pressed:
-        return self.get_tree().change_scene("res://scenes/HowTo.tscn")
+	if event is InputEventMouseButton and event.pressed:
+		return self.get_tree().change_scene("res://scenes/HowTo.tscn")
 
-    if event is InputEventJoypadButton and event.pressed:
-        return self.get_tree().change_scene("res://scenes/HowTo.tscn")
+	if event is InputEventJoypadButton and event.pressed:
+		return self.get_tree().change_scene("res://scenes/HowTo.tscn")
 
-    if event is InputEventKey:
-        return self.get_tree().change_scene("res://scenes/HowTo.tscn")
+	if event is InputEventKey:
+		return self.get_tree().change_scene("res://scenes/HowTo.tscn")
