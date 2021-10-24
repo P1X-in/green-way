1fbadf9fe43b2dd59d959667ebf5aa4ee526ce17
Krzysztof Jankowski <krzysztof.jankowski@beffio.com>
2021-10-24 14:18:19 +0200

    final build settings and UI layout

--- scenes/screens/HowTo.gd
+++ scenes/screens/HowTo.gd
@@ -1,15 +1,15 @@
 extends Control
 
 func _input(event):
-    if Input.is_key_pressed(KEY_ESCAPE):
-        get_tree().quit()
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
 
-    if event is InputEventMouseButton and event.pressed:
-        return self.get_tree().change_scene("res://scenes/board/board.tscn")
+	if event is InputEventMouseButton and event.pressed:
+		return self.get_tree().change_scene("res://scenes/board/board.tscn")
 
-    if event is InputEventJoypadButton and event.pressed:
-        return self.get_tree().change_scene("res://scenes/board/board.tscn")
+	if event is InputEventJoypadButton and event.pressed:
+		return self.get_tree().change_scene("res://scenes/board/board.tscn")
 
-    if event is InputEventKey and event.pressed and not event.echo:
-        return self.get_tree().change_scene("res://scenes/board/board.tscn")
+	if event is InputEventKey and event.pressed and not event.echo:
+		return self.get_tree().change_scene("res://scenes/board/board.tscn")
 
